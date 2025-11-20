b935c26cc88ad88a3624e6606cb35c979d5f3f89
Mohamed Salama <slamtm608@gmail.com>
2025-11-21 00:16:40 +0200

    models and migrations post and comment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,22 @@ class Post extends Model
 {
     //
     
+    protected $fillable = [
+        'title',
+        'content',
+        'author_id',
+        'category',
+    ];
+
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
 }
